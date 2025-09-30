2ae9b820928976fe4139ef6bcd4613b00d2b7d75
Arslan <chemchiev@gmail.com>
2025-09-30 01:40:33 +0100

    commit from NeatBeans: added Student.java

---
+++ src/main/java/git/week5project/Student.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package git.week5project;
+
+/**
+ *
+ * @author chemc
+ */
+public class Student {
+    private String name;
+    private String age;
+    private String studentnumber;
+    
+    Student(String given_name){
+        name = given_name;
+    }
+    
+}
